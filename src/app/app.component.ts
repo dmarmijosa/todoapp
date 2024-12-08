12690189e8523d5dcc8a0c2581fba7e31cb04028
Danny Armijos <daniel2667@hotmail.es>
2024-12-08 20:18:05 +0100

    mejora de cambios y conrol de versiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'todoapp';
   welcome = 'Bienvenido a mi primera aplicación de tareas';
   task = [
@@ -17,4 +18,9 @@ export class AppComponent {
     'Crear componente',
     'Crear Servicio',
   ];
+
+  ngOnInit(): void {
+    console.log(`Entorno de publicación: ${environment.entorno} Versión: 1.0.0`);
+  }
+  
 }
